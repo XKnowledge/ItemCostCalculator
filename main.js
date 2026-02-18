b1085ee171128fbf7176745927c25d330594b94f
FishPotatoChen <FishPotatoChen@163.com>
2026-02-18 13:06:06 +0800

    框架构建

---
+++ main.js
@@ -0,0 +1,84 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 700,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// 获取应用数据路径
+function getAppDataPath() {
+  return __dirname;
+}
+
+// 保存数据到文件
+function saveData(data) {
+  const filePath = path.join(getAppDataPath(), 'data.json');
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+}
+
+// 读取数据
+function loadData() {
+  const filePath = path.join(getAppDataPath(), 'data.json');
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath);
+    return JSON.parse(data);
+  }
+  return { items: [] };
+}
+
+// IPC 处理
+ipcMain.handle('save-item', (event, item) => {
+  const data = loadData();
+  data.items.push(item);
+  saveData(data);
+  return true;
+});
+
+ipcMain.handle('get-items', () => {
+  return loadData().items;
+});
+
+ipcMain.handle('update-item', (event, item) => {
+  const data = loadData();
+  const index = data.items.findIndex(i => i.id === item.id);
+  if (index !== -1) {
+    data.items[index] = item;
+    saveData(data);
+  }
+  return true;
+});
+
+ipcMain.handle('delete-item', (event, id) => {
+  const data = loadData();
+  data.items = data.items.filter(item => item.id !== id);
+  saveData(data);
+  return true;
+});
---
+++ preload.js
@@ -0,0 +1,8 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  saveItem: (item) => ipcRenderer.invoke('save-item', item),
+  getItems: () => ipcRenderer.invoke('get-items'),
+  updateItem: (item) => ipcRenderer.invoke('update-item', item),
+  deleteItem: (id) => ipcRenderer.invoke('delete-item', id)
+});
